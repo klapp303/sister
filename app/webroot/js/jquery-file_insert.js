549818b07b9ec51359ebb842fb5092bf5140af81
klapp303 <klapp303@gmail.com>
2016-07-20 13:37:03 +0900

    管理画面の日記作成ページで画像の挿入コードを年月ベースの新しいディレクトリ構造に対応させる

--- app/webroot/js/jquery-file_insert.js
+++ app/webroot/js/jquery-file_insert.js
@@ -25,6 +25,13 @@ jQuery(function($) {
             alert('画像ファイルを選んでください');
             return false;
         }
-        $('.js-insert_area').insertAtCaret('<img src="/files/photo/' + img_name + '" alt="" class="img_diary">');
+        var date = new Date();
+        
+        //年月ベースの画像URLを設定するため
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        var month = ('0' + month).slice(-2);
+        
+        $('.js-insert_area').insertAtCaret('<img src="/files/photo/' + year + '/' + month + '/' + img_name + '" alt="" class="img_diary">');
     });
 });
